5e4e752e4c0e5b6f1b57b7fc89eba6960cd3ceec
Srinu Rachakonda <srinurachakonda126@gmail.com>
2024-08-25 09:55:12 -0400

    Updated Landing page

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -6,8 +7,14 @@ const Header = () => {
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link to="/">
-          <img src="/logo.png" className="h-20" />
+          <img src="/logo.png" className="h-20" alt="Hired Logo" />
         </Link>
+        {/* <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn> */}
         <Button variant="outline">Login</Button>
       </nav>
     </>
